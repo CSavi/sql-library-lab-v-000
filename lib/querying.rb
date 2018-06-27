98ac4a3d8ee0636e2e0e6e59c7df9a5ea262c817
Carrie Savinell <carrielane89@gmail.com>
2018-06-27 23:57:33 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -33,7 +33,7 @@ def select_series_title_with_most_human_characters
    FROM series
    JOIN characters
    ON series.id = characters.series_id
-   WHERE MAX(characters.species) IS human"
+   WHERE MAX(characters.species) IS human;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
